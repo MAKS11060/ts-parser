fc0c2cee20173074f32a0768e3b52ef68c089315
MAKS11060 <31521952+MAKS11060@users.noreply.github.com>
2026-06-05 08:49:40 +0200

    upd

--- packages/tracker/src/rutracker.test.ts
+++ packages/tracker/src/rutracker.test.ts
@@ -1,36 +1,34 @@
 import test from 'node:test'
 import {RuTracker} from './rutracker.ts'
 
-test('Test 210891', async (t) => {
-  const ruTracker = new RuTracker({session: process.env.RUTRACKER_SESSION!})
-  const res = await ruTracker.search({query: ''})
+const ruTracker = new RuTracker({
+  session: process.env.RUTRACKER_SESSION!,
+  async fetch(input, init) {
+    console.log('proxy', input)
+    const request = new Request(input, init)
+    const res = await fetch(`https://proxy.maks11060.workers.dev/${request.url}`, request)
+    return res
+  },
+})
 
+test('Test 210891', async (t) => {
+  const res = await ruTracker.search({query: ''})
   console.dir(res, {depth: null})
 })
 
 test('Test 591098', async (t) => {
-  const ruTracker = new RuTracker({session: process.env.RUTRACKER_SESSION!})
   const res = await ruTracker.download({id: 6842981})
   console.log(res.response)
   console.log(res.getFile())
 })
 
 test('Test 591011', async (t) => {
-  const ruTracker = new RuTracker({session: process.env.RUTRACKER_SESSION!})
   const res = await ruTracker.view({id: 6842981})
   console.log(res)
 })
 
 test('Test 291087', async (t) => {
-  const ruTracker = new RuTracker({session: process.env.RUTRACKER_SESSION!})
-  const res = await ruTracker.viewTorrent({
-    id: 6842981,
-    async fetch(input, init) {
-      const request = new Request(input, init)
-      const res = await fetch(`https://proxy.maks11060.workers.dev/${request.url}`, request)
-      return res
-    },
-  })
+  const res = await ruTracker.viewTorrent({id: 6842981})
   console.log(res.response)
   console.dir(res.fileList, {depth: null})
 })
